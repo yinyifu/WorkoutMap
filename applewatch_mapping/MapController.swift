9d1cce76871b9fcb554c1b9448d273a206778099
IOS Coding <iosdesigncoding@yifus-mbp.fios-router.home>
2017-09-08 05:12:33 -0400

    First File added, overview for how map works

---
+++ applewatch_mapping/MapController.swift
@@ -0,0 +1,72 @@
+//
+//  MapController.swift
+//  applewatch_mapping
+//
+//  Created by IOS Design Coding on 9/8/17.
+//  Copyright © 2017 CSE442_UB. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import UIKit
+
+class MapController : CLLocationManagerDelegate{
+
+    
+    /*************************
+     *   Reference: https://stackoverflow.com/questions/25296691/get-users-current-location-coordinates
+     */
+    let locationManager = CLLocationManager()
+    func MapConInit(){
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    /*************************/
+   
+    func getUserCurrentLocation() -> CLLocation
+    {
+        
+        return locationManager.location.coordinate;
+    }
+    func showSuggestions (input : NSString) -> NSArray<NSString>
+    {
+        
+    }
+    func searchUserDestination(input : NSString) -> NSArray<CLLocation>
+    {
+    
+    }
+    
+    func routingBetweenLocations(from : CLLocation, to : CLLocatios) -> NSObject?
+    {
+    
+    }
+    
+    func getLocationDescriptions (place : CLLocation)->NSString
+    {
+    
+    }
+    
+    func sendDataToWatch(watch/*: watch?*/)
+    {
+    
+    }
+
+    func retreveDataFromWatch(watch) -> NSString
+    {
+        
+    }
+    
+    func ifDirectionChange(/*routing data*/)->Bool
+    {
+    }
+    
+    func directionChange(/*routing data*/) /* -> direction data         */{
+        
+    }
+}
