591ab9a5042abc4269709800357a35ffcd6b151a
Yifu Yin <yifuyin@buffalo.edu>
2017-10-23 18:58:43 -0400

    Revert "Mapdirect"

--- applewatch_mapping/LocationManagerController.swift
+++ applewatch_mapping/LocationManagerController.swift
@@ -9,43 +9,44 @@
 import Foundation
 import CoreLocation
 import WatchConnectivity
-import GoogleMaps
+import MapKit
 
-class LocationManagerController : NSObject, CLLocationManagerDelegate{
-    var _mapview : GMSMapView?;
+class LocationManagerController : NSObject, CLLocationManagerDelegate, locationDelegate{
+    var _mapview : MKMapView?;
     let _square_window : Double = 0.001
-    let cm = CLLocationManager();
-    init(mapview:GMSMapView){
-        super.init()
-        self._mapview = mapview;
-        if(!CLLocationManager.locationServicesEnabled()) {
-            print("error mother fuckboy not enabled")
-        }
-        if CLLocationManager.authorizationStatus() == .notDetermined {
-            self.cm.requestWhenInUseAuthorization()
-        }
-        if((CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways )||(CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse)){
-            cm.desiredAccuracy = kCLLocationAccuracyBest;
-            cm.delegate = self
-            cm.startUpdatingLocation()
-        }else if (CLLocationManager.authorizationStatus() == .denied){
-            NSLog("error motherfucker");
-        }
+    func onUserLocationChange() {
+        // do the ui update
         
+        
+        
+        
+        // do the watch communication
+    }
+    
+    func onUserChosenLocationChange() {
+        //update route thing
+        
+        // ^
+    }
+    
+    func onUserDirectionLocationChange() {
+        
+    }
+    
+    override init(){
+        super.init();
+    }
+    init(mapview:MKMapView){
+        self._mapview = mapview;
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
         if let use = locations.last{
-            if let map = self._mapview{
-                map.camera = GMSCameraPosition.camera(withLatitude: use.coordinate.latitude, longitude: use.coordinate.longitude, zoom: 16.0);
-            }
+            
+            self._mapview?.setRegion(MKCoordinateRegion(center: use.coordinate, span: MKCoordinateSpan(latitudeDelta: _square_window, longitudeDelta: _square_window )), animated: true)
         }
     }
-    func locationManager(_ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?) {
-        NSLog("dick mother")
-    }
-    
     func didChangeValue<Value>(for keyPath: KeyPath<LocationManagerController, Value>) {
         NSLog("asdffdsa")
     }
--- applewatch_mapping/ViewController.swift
+++ applewatch_mapping/ViewController.swift
@@ -9,33 +9,96 @@
 import UIKit
 import MapKit
 import CoreLocation
-import GoogleMaps
-import GooglePlaces
 
 //@interface MyLocationViewController : UIViewController <CLLocationManagerDelegate>
 
 class ViewController: UIViewController {
-    let _sc : SessionController = SessionController();
-    private var mapController : MapController?;
-    @IBAction func autocompleteClicked(_ sender: UIButton) {
-        let autocompleteController = GMSAutocompleteViewController()
-        autocompleteController.delegate = self
-        present(autocompleteController, animated: true, completion: nil)
-    }
-   
-    override func viewDidLoad() {
-        super.viewDidLoad();
-    }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? MapController,
-            segue.identifier == "goMapSegue" {
-            self.mapController = vc
-        }else{
-            NSLog("Motherfucker didnt prepare");
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var longLabel: UILabel!
+    @IBOutlet weak var rotation: UILabel!
+    @IBOutlet weak var addresslabel: UILabel!
+    @IBOutlet weak var mapview: MKMapView!
+    var mapCon:MapController = MapController();
+    var sc : SessionController = SessionController();
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.addTarget(self, action: #selector(self.getLoc(sender:event:)), for: .touchUpInside)
+        mapview.delegate = mapCon;
+        let mkc : MKCoordinateRegion = MKCoordinateRegionMake(CLLocationCoordinate2D(latitude: 30, longitude: 80), MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.2));
+        mapview.setRegion(mkc, animated: true)
+        // Do any additional setup after loading the view, typically from a nib.
+        //sc.send_image();
+        mapCon.map_prepare(mapview: mapview)
+        do{
+            try mapCon.startGettingLocations()
+        }catch(LocationException.authorizationDenied){
+            NSLog("error: authorization denied")
+        }catch(LocationException.authorizationRestricted){
+            NSLog("error: authorization restricted")
+        }catch(LocationException.authorizationUndetermined){
+            NSLog("error: authorization udetermined")
+        }catch(LocationException.locationServiceNotEnabled){
+            NSLog("error: location not enable")
+        }catch(LocationException.locDelNotSet){
+            NSLog("error: no deleagte for locations")
+        }catch(LocationException.mapViewNotSet){
+            NSLog("error: no map view")
+        }catch{
+            NSLog("error: some error idk")
         }
+    }
+    @IBAction func getLocation(sender: UIButton, event: UIEvent){
+        do {
+            let userLoc: CLLocation? = try self.mapCon.getUserCurrentLocation();
+           /* let coor : CLLocationCoordinate2D = userLoc!.coordinate;
+            let lat :CLLocationDegrees = coor.latitude
+            let long :CLLocationDegrees = coor.longitude
+            longLabel.text = String(lat)
+            latLabel.text  = String(long)
+             */
+            if let coor:CLLocationCoordinate2D = userLoc?.coordinate{
+                mapview.setCenter(coor, animated: true);
+            }
+            
+        }catch(LocationException.authorizationDenied){
+            print("Some means to error handling")
+        }
+        catch(LocationException.authorizationRestricted){}
+        catch(LocationException.authorizationUndetermined){}
+        catch{}
         
     }
+    @objc func get_c(sender: UIButton, event: UIEvent){
+        do {
+            let userLoc: CLLocation? = try self.mapCon.getUserCurrentLocation();
+            if(userLoc == nil){
+                longLabel.text = String("nil")
+                alerting(title : "nil", message : "why are you nil?");
+            }
+            if userLoc != nil{
+                let coor : CLLocationCoordinate2D = userLoc!.coordinate;
+                let lat :CLLocationDegrees = coor.latitude
+                let long :CLLocationDegrees = coor.longitude
+                longLabel.text = String(lat)
+                latLabel.text  = String(long)
+                //mapview.setCenter(coor, animated: true)
+            }
+        }catch(LocationException.authorizationDenied){
+            longLabel.text = String("denied")
+        }
+        catch(LocationException.authorizationRestricted){
+            longLabel.text = String("restri")
+        }
+        catch(LocationException.authorizationUndetermined){
+            longLabel.text = String("undeter")
+        }
+        catch{
+            longLabel.text = String("other")
+        }
+    }
+    
     func alerting(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle : UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
@@ -43,144 +106,19 @@ class ViewController: UIViewController {
     }
     //trying to make uiimage
     @objc func getLoc(sender: UIButton, event: UIEvent){
-        _sc.send_image()
+        sc.send_image()
     }
-}
-
-
-extension ViewController : GMSAutocompleteViewControllerDelegate {
-    
-    // Handle the user's selection.
-    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        // runner.setCenter(place.coordinate)
-        //let sboar : UIStoryboard = UIStoryboard(name:"Main", bundle:nil);
-        if let map = self.mapController{
-            map.setCenter(place.coordinate);
-        }else{
-            NSLog("Motherfucker didnt coord");
-        }
-        NSLog("coor is \(place.coordinate.latitude) + \(place.coordinate.longitude)");
-        NSLog("runner is nil");
-        dismiss(animated: true, completion: nil)
-        
-    }
-    
-    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-        // TODO: handle the error.
-        print("Error: ", error.localizedDescription)
-    }
-    
-    // User canceled the operation.
-    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // Turn the network activity indicator on and off again.
-    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-    }
-    
-    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
-    }
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let camera = GMSCamera
-//let singleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.switchView))
-//singleTap.numberOfTapsRequired = 1 // you can change this value
-//image.isUserInteractionEnabled = true;
-//image.addGestureRecognizer(singleTap);
-
-/*mapview.delegate = mapCon;
- let mkc : MKCoordinateRegion = MKCoordinateRegionMake(CLLocationCoordinate2D(latitude: 30, longitude: 80), MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.2));
- mapview.setRegion(mkc, animated: true)
- // Do any additional setup after loading the view, typically from a nib.
- //sc.send_image();
- mapCon.map_prepare(mapview: mapview)
- do{
- try mapCon.startGettingLocations()
- }catch(LocationException.authorizationDenied){
- alerting(title : "Authorization Denied", message : "Map service needs your location info.");
- locationMan.requestAlwaysAuthorization();
- }catch(LocationException.authorizationRestricted){
- NSLog("error: authorization restricted")
- }catch(LocationException.authorizationUndetermined){
- NSLog("error: authorization udetermined")
- }catch(LocationException.locationServiceNotEnabled){
- NSLog("error: location not enable")
- }catch(LocationException.locDelNotSet){
- NSLog("error: no deleagte for locations")
- }catch(LocationException.mapViewNotSet){
- NSLog("error: no map view")
- }catch{
- NSLog("error: some error idk")
- }*/
-/*}
- @IBAction func getLocation(sender: UIButton, event: UIEvent){
- do {
- let userLoc: CLLocation? = try self.mapCon.getUserCurrentLocation();
- if let coor:CLLocationCoordinate2D = userLoc?.coordinate{
- mapview.setCenter(coor, animated: true);
- }
- 
- }catch(LocationException.authorizationDenied){
- print("Some means to error handling")
- }
- catch(LocationException.authorizationRestricted){}
- catch(LocationException.authorizationUndetermined){}
- catch{}
- 
- }
- @objc func get_c(sender: UIButton, event: UIEvent){
- do {
- let userLoc: CLLocation? = try self.mapCon.getUserCurrentLocation();
- if(userLoc == nil){
- longLabel.text = String("nil")
- alerting(title : "nil", message : "why are you nil?");
- }
- if userLoc != nil{
- let coor : CLLocationCoordinate2D = userLoc!.coordinate;
- let lat :CLLocationDegrees = coor.latitude
- let long :CLLocationDegrees = coor.longitude
- longLabel.text = String(lat)
- latLabel.text  = String(long)
- //mapview.setCenter(coor, animated: true)
- }
- }catch(LocationException.authorizationDenied){
- longLabel.text = String("denied")
- }
- catch(LocationException.authorizationRestricted){
- longLabel.text = String("restri")
- }
- catch(LocationException.authorizationUndetermined){
- longLabel.text = String("undeter")
- }
- catch{
- longLabel.text = String("other")
- }
- }
- */
-
-
 /*
- override func didReceiveMemoryWarning() {
- super.didReceiveMemoryWarning()
- // Dispose of any resources that can be recreated.
- }
- */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+*/
+
+}
+
+
+
 
            
 
